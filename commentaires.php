5a395fc9cb3a75c5104d44b2da5930039bcbccb0
farrah <marzini.f@gmail.com>
2017-09-15 10:16:07 +0200

    pseudo code written

--- commentaires.php
+++ commentaires.php
@@ -6,7 +6,17 @@
   </head>
   <body>
 
-    
+    //pseudo code
+    //faire requete pour lier la BDD article a cette page
+    // identifier le ID pour savoir quel article afficher
+    // lafficher
+    // fermer cette requete
+
+    // faire requete pour lier la BDD commentaire a cette page
+    // identifier le ID pour savoir quels commentaires afficher
+    // créer une boucle qui selectionne et affiche tous les commentaires qui possède l'id envoyé par l'url
+    // fermer cette requete
+
 
   </body>
 </html>
--- index.php
+++ index.php
@@ -5,6 +5,11 @@
     <title>Mon mini mini mini Blog</title>
   </head>
   <body>
+//pseudo code
+//faire requete pour lier la BDD a cette page
+//faire une boucle pour afficher tous les articles (5 derniers orderby desc + limit)
+// dans cette boucle y mettre le bouton commentaires pour rediriger vers page commentaires avec info de l'id de l'article dans url
+// fermer cette requete
 
   </body>
 </html>
