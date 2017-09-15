76ce663e6ae916f4af5d787f4739b9da26983c44
farrah <marzini.f@gmail.com>
2017-09-15 11:30:24 +0200

    index.php ok

--- index.php
+++ index.php
@@ -3,6 +3,45 @@
   <head>
     <meta charset="utf-8">
     <title>Mon mini mini mini Blog</title>
+    <style media="screen">
+    h1, h2
+    {
+      text-align:center;
+    }
+
+    h2
+    {
+      background-color:black;
+      color:white;
+      font-size:0.9em;
+      position: relative;
+      margin-bottom:0px;
+      height: 40px;
+    }
+
+    h2 span {
+      position: absolute;
+      right: 0;
+      color: white!important;
+    }
+
+    .news p
+    {
+      background-color:#CCCCCC;
+      margin-top:0px;
+    }
+    .news
+    {
+      width:70%;
+      margin:auto;
+    }
+
+    a
+    {
+      text-decoration: none;
+      color: blue;
+    }
+    </style>
   </head>
   <body>
 
@@ -21,32 +60,32 @@ catch(Exception $e)
 }
 
 //OK faire une requete pour afficher tous les articles (5 derniers orderby desc + limit)
-$mesarticles = $bdd->query('SELECT * FROM billets ORDER BY ID DESC LIMIT 0, 5') or die(print_r($bdd->errorInfo()));
+$mesarticles = $bdd->query('SELECT ID, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y %Hh%imin%ss\') AS ladate FROM billets ORDER BY ID DESC LIMIT 0, 5') or die(print_r($bdd->errorInfo()));
 
 //OK faire une boucle pour afficher la requete
 while ($article = $mesarticles->fetch())
 {
 ?>
 
-<!-- mettre html -->
-<article>
+  <!-- OK mettre html -->
+  <article class="news">
 
-<h2>Y mettre mon titre d'article</h2>
-<h3>Ecrit le y mettre la date  et l'heure </h3>
-<p>Y mettre larticle</p>
+    <h2><?= $article['titre'] ?> <br>
+      <span>Le <?= $article['ladate'] ?> </span>
+    </h2>
+    <p><?= $article['contenu'] ?><br>
+      <!-- OK dans cette boucle y mettre le bouton commentaires pour rediriger vers page commentaires avec info de l'id de l'article dans url -->
+      <a href="commentaires.php?ID=<?=$article['ID']?>">Lire les commentaires ! </a>
+    </p>
 
-</article>
+  </article>
 
 <?php
 } // fin de ma boucle while
-// {
-// 	echo '<li>' . $donnees['nom'] . ' (' . $donnees['prix'] . ' EUR)</li>';
-// }
 
 //OK fermer cette requete
 $mesarticles->closeCursor();
 
-// dans cette boucle y mettre le bouton commentaires pour rediriger vers page commentaires avec info de l'id de l'article dans url
 
 ?>
 </main>
