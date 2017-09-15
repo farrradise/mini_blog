cee55208ee236183eca7464d949577f42e6e1a60
farrah <marzini.f@gmail.com>
2017-09-15 10:57:06 +0200

    started to translate the pseudo code in real code

--- index.php
+++ index.php
@@ -5,11 +5,51 @@
     <title>Mon mini mini mini Blog</title>
   </head>
   <body>
-//pseudo code
-//faire requete pour lier la BDD a cette page
-//faire une boucle pour afficher tous les articles (5 derniers orderby desc + limit)
+
+<h1>Mon BLOG de fifou</h1>
+<main>
+
+<?php
+// OK faire requete pour lier la BDD a cette page
+try
+{
+  $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+  die('Erreur : '.$e->getMessage());
+}
+
+//OK faire une requete pour afficher tous les articles (5 derniers orderby desc + limit)
+$mesarticles = $bdd->query('SELECT * FROM billets ORDER BY ID DESC LIMIT 0, 5') or die(print_r($bdd->errorInfo()));
+
+//OK faire une boucle pour afficher la requete
+while ($article = $mesarticles->fetch())
+{
+?>
+
+<!-- mettre html -->
+<article>
+
+<h2>Y mettre mon titre d'article</h2>
+<h3>Ecrit le y mettre la date  et l'heure </h3>
+<p>Y mettre larticle</p>
+
+</article>
+
+<?php
+} // fin de ma boucle while
+// {
+// 	echo '<li>' . $donnees['nom'] . ' (' . $donnees['prix'] . ' EUR)</li>';
+// }
+
+//OK fermer cette requete
+$mesarticles->closeCursor();
+
 // dans cette boucle y mettre le bouton commentaires pour rediriger vers page commentaires avec info de l'id de l'article dans url
-// fermer cette requete
+
+?>
+</main>
 
   </body>
 </html>
